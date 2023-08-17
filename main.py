e614135633121d1d636c682286ab1d533d6db4bc
Andre Santos <andrepinhos@gmail.com>
2023-08-17 18:25:34 +0100

    Work on adding components to box 1

--- main.py
+++ main.py
@@ -13,11 +13,13 @@ frame.pack(fill="both", expand=True)
 bframe = tk.Frame(window)
 bframe.pack(side="bottom")
 
+cam = ["CAM1","CAM2","CAM3","CAM4","CAM5","CAM6","CAM7","CAM8","CAM9","CAM10","CAM11","CAM12","CAM13","CAM14","CAM15"]
+
 #Main Menu
 menu = tk.Menu(window)
 #File Menu
 file_menu = tk.Menu(menu, tearoff = False)
-file_menu.add_command(label="Quit", command = lambda: quit())
+file_menu.add_command(label="Quit", command = lambda: quit)
 menu.add_cascade(label = "File", menu = file_menu)
 window.configure(menu = menu)
 
@@ -37,11 +39,16 @@ bottom_main_frame.pack(side="bottom", padx=5)
 #Right Menu
 right_menu_style = ttk.Style()
 right_menu_style.configure("rmenu.TNotebook", font=("Open Sans",12), background="light grey")
-
 right_menu=ttk.Notebook(right_main_frame, style="rmenu.TNotebook")
 
-
+#Box 1
 tab_box1 = tk.Frame(right_menu, background="light grey", width=302, height=525)
+enable_box1 = tk.Checkbutton(tab_box1, text="Enable", font=("Open Sans",12), background="light grey", foreground="black")
+enable_box1.pack(side="left")
+list_box1 = tk.Listbox(tab_box1, selectmode="single", )
+
+
+
 tab_box2 = tk.Frame(right_menu, background="light grey", width=302, height=525)
 tab_box3 = tk.Frame(right_menu, background="light grey", width=302, height=525)
 tab_box4 = tk.Frame(right_menu, background="light grey", width=302, height=525)
